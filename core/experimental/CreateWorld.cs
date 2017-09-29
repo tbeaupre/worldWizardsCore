657d29bcf75dc94c6cd3de491de8cc325018fb39
Hope Wallace <wallacehope1@gmail.com>
2017-09-29 12:39:49 -0400

    Basic working VR application.

---
+++ core/experimental/CreateWorld.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using worldWizards.core.controller.level.utils;
+using worldWizards.core.controller.level;
+using worldWizards.core.entity.coordinate;
+using UnityEngine;
+
+
+
+namespace worldWizards.core.experimental { 
+public class CreateWorld : MonoBehaviour {
+
+    // Use this for initialization
+    void Start() {
+
+            SceneGraphController sceneGraphController = FindObjectOfType<SceneGraphController>();
+            WWResourceController.LoadResource("white", null, "whiteCube");
+            WWResourceController.LoadResource("tree", null, "treeProp");
+
+            string imagePath = "Heightmaps/MazeHeightmap";
+            Texture2D heightmap = Resources.Load<Texture2D>(imagePath);
+
+            List<Coordinate> terrainCoordinates = TerrainGenerator.CreateTerrainFromImage(sceneGraphController, heightmap);
+
+        }
+
+
+}
+}
---
+++ core/viveControllers/ShowMenu.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using worldWizards.core.controller.level.utils;
+using worldWizards.core.controller.level;
+using worldWizards.core.entity.coordinate;
+using worldWizards.core.entity.coordinate.utils;
+using worldWizards.core.entity.gameObject;
+using UnityEngine;
+
+public class ShowMenu : MonoBehaviour {
+
+    private SteamVR_TrackedObject trackedObj;
+    private SceneGraphController sceneGraphController;
+    private GameObject menu;
+    private Transform menuTransform;
+    public Transform cameraRigTransform;
+    public Transform headTransform;
+    public Camera headCamera;
+
+    private bool isMenuActive;
+
+    private SteamVR_Controller.Device Controller
+    {
+        get { return SteamVR_Controller.Input((int)trackedObj.index); }
+    }
+
+    void Awake()
+    {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+        menu = GameObject.Find("Menu");
+        menuTransform = menu.transform;
+    }
+
+    void Start()
+    {
+        sceneGraphController = FindObjectOfType<SceneGraphController>();
+
+        isMenuActive = false;
+        menu.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isMenuActive)
+        {
+            // Change position of the menu based on player head position
+            //menuTransform.position = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width / 2, Screen.height / 2, headCamera.nearClipPlane));
+            //menuTransform.LookAt(headTransform);
+        }
+
+        if (Controller.GetPressDown(SteamVR_Controller.ButtonMask.ApplicationMenu))
+        {
+            if (!isMenuActive)
+            {
+                ShowMainMenu();
+            }
+            else
+            {
+                OnOK();
+            }
+        }
+    }
+
+    void ShowMainMenu()
+    {
+        isMenuActive = true;
+        menu.SetActive(true);
+        Debug.Log("On show, menu active: " + menu.activeInHierarchy);
+    }
+
+    void OnOK()
+    {
+        isMenuActive = false;
+        menu.SetActive(false);
+        Debug.Log("On hide, menu active: " + menu.activeInHierarchy);
+    }
+}
