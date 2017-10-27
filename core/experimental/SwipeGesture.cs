d1811d587c402a90baf518097279458cf652d7b6
Tyler Beaupre <tylerbeaupre4@gmail.com>
2017-10-27 18:26:45 -0400

    Swipe Gesture

---
+++ core/experimental/SwipeGesture.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace WorldWizards.core.experimental
+{
+    public class SwipeGesture
+    {
+        private bool trackingSwipe = false;
+        private Vector2 startPosition;
+	
+        public float GetSwipeRatio (SteamVR_Controller.Device controller) {
+            // Swipe
+            if (controller.GetTouchDown(Valve.VR.EVRButtonId.k_EButton_Axis0)) // Was up, is down.
+            {
+                trackingSwipe = true;
+                startPosition = controller.GetAxis(Valve.VR.EVRButtonId.k_EButton_Axis0);
+            }
+            else if (controller.GetTouchUp(Valve.VR.EVRButtonId.k_EButton_Axis0)) // Was down, is up.
+            {
+                trackingSwipe = false;
+            }
+            if (trackingSwipe)
+            {
+                Vector2 curPosition = controller.GetAxis(Valve.VR.EVRButtonId.k_EButton_Axis0);
+                float deltaX = curPosition.x - startPosition.x;
+
+                return deltaX / 2;
+            }
+            
+            return 0;
+        }
+    }
+}
