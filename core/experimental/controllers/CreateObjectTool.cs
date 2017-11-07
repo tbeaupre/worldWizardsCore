65c0afcc4c0bdcae24e62aa5850acad5888bf017
Tyler Beaupre <tylerbeaupre4@gmail.com>
2017-11-07 16:22:54 -0500

    Example Tools

---
+++ core/experimental/controllers/CreateObjectTool.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEditor.Callbacks;
+using UnityEngine;
+using WorldWizards.core.controller.level;
+using WorldWizards.core.controller.level.utils;
+using WorldWizards.core.entity.coordinate.utils;
+using WorldWizards.core.entity.gameObject;
+
+namespace WorldWizards.core.experimental.controllers
+{
+    public class CreateObjectTool : Tool
+    {
+        private readonly SwipeGesture swipe = new SwipeGesture();
+        
+        // Controllers
+        private SceneGraphController sceneGraphController;
+        
+        // Prefabs
+        public Collider gridCollider;
+        
+        // Object Properties
+        private WWObject curObject;
+        private int curRotation;
+        private int curTileIndex;
+        
+        // Resources
+        private List<string> possibleTiles;
+
+        // Raycast Information
+        private bool validTarget = false;
+        private Vector3 hitPoint;
+        
+        protected override void Awake()
+        {
+            base.Awake();
+            trigger = true;
+            grip = true;
+            appMenu = true;
+            press = true;
+            touch = true;
+            
+            sceneGraphController = FindObjectOfType<SceneGraphController>();
+            
+            ResourceLoader.LoadResources(); // Should be removed at some point.
+            possibleTiles = new List<string>(WWResourceController.bundles.Keys);
+            
+            gridCollider.transform.localScale = Vector3.one * CoordinateHelper.tileLengthScale;
+        }
+
+        protected override void Update()
+        {
+            RaycastHit raycastHit;
+            if (Physics.Raycast(trackedObj.transform.position, transform.forward, out raycastHit, 100))
+            {
+                validTarget = true;
+                hitPoint = raycastHit.point;
+                
+                // because the tile center is in the middle need to offset
+                hitPoint.x += .5f * CoordinateHelper.baseTileLength * CoordinateHelper.tileLengthScale;
+                hitPoint.y += CoordinateHelper.baseTileLength * CoordinateHelper.tileLengthScale;
+                hitPoint.z += .5f * CoordinateHelper.baseTileLength * CoordinateHelper.tileLengthScale;
+            }
+            else
+            {
+                validTarget = false;
+            }
+            
+            base.Update();
+        }
+        
+        private WWObject PlaceObject(Vector3 position)
+        {
+            var coordinate = CoordinateHelper.convertUnityCoordinateToWWCoordinate(position, curRotation);
+            var objData = WWObjectFactory.CreateNew(coordinate, possibleTiles[curTileIndex]);
+            var gameObj = WWObjectFactory.Instantiate(objData);
+            return gameObj;
+        }
+
+        
+        // Trigger
+        protected override void OnTrigger()
+        {
+            if (validTarget)
+            {
+                if (curObject != null)
+                {
+                    Destroy(curObject.gameObject);
+                    curObject = PlaceObject(hitPoint);
+                    sceneGraphController.Add(curObject);
+                    curObject = null;
+                }
+            }
+        }
+
+        protected override void UpdateTrigger()
+        {
+            if (validTarget)
+            {
+                if (curObject == null)
+                {
+                    curObject = PlaceObject(hitPoint);
+                }
+                else
+                {
+                    curObject.transform.position = new Vector3(hitPoint.x,
+                        hitPoint.y + 0.5f * CoordinateHelper.baseTileLength * CoordinateHelper.tileLengthScale,
+                        hitPoint.z);
+                }
+            }
+        }
+
+        
+        // Grip
+        protected override void OnGrip()
+        {
+            RaycastHit raycastHit;
+            if (Physics.Raycast(trackedObj.transform.position, transform.forward, out raycastHit, 100))
+            {
+                WWObject wwObject = raycastHit.transform.gameObject.GetComponent<WWObject>();
+                if (!wwObject.Equals(curObject))
+                {
+                    sceneGraphController.Delete(wwObject.GetId());
+                }
+            }
+        }
+        
+        
+        // Touchpad Press
+        protected override void OnTouchpadPress(Vector2 pos)
+        {
+            // Rotation
+            if (validTarget && curObject != null)
+            {
+                if (pos.x < -DEADZONE_SIZE)
+                {
+                    curRotation += 90;
+                    Destroy(curObject.gameObject);
+                    curObject = PlaceObject(hitPoint);
+                }
+                if (pos.x > DEADZONE_SIZE)
+                {
+                    curRotation -= 90;
+                    Destroy(curObject.gameObject);
+                    curObject = PlaceObject(hitPoint);
+                }
+            }
+
+            // Move Grid
+            Vector3 gridPosition = gridCollider.transform.position;
+            if (pos.y > DEADZONE_SIZE)
+            {
+                gridPosition.y += CoordinateHelper.baseTileLength * CoordinateHelper.tileLengthScale;
+            }
+            if (pos.y < -DEADZONE_SIZE)
+            {
+                gridPosition.y -= CoordinateHelper.baseTileLength * CoordinateHelper.tileLengthScale;
+            }
+            gridCollider.transform.position = gridPosition;
+        }
+        
+        
+        // Touchpad Touch
+        protected override void UpdateTouchpadPress(Vector2 pos)
+        {
+            if (curObject != null)
+            {
+                var offset = (int)(possibleTiles.Count * swipe.GetSwipeRatio(Controller));
+                if (offset != 0)
+                {
+                    curTileIndex = (curTileIndex + offset) % possibleTiles.Count;
+                    Destroy(curObject.gameObject);
+                    curObject = PlaceObject(hitPoint);
+                }
+            }
+        }
+    }
+}
---
+++ core/experimental/controllers/StandardTool.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using UnityEngine;
+
+namespace WorldWizards.core.experimental.controllers
+{
+    public class StandardTool : Tool
+    {
+        private const float RETICLE_OFFSET = 0.001f; // The reticle offset from the floor
+        private const float MOVE_OFFSET = 0.1f; // The distance the player moves per frame.
+        
+        // Prefabs
+        public GameObject laserPrefab;
+        public GameObject teleportReticlePrefab; // Teleport reticle prefab
+        
+        // VR Headset Transform Information
+        private Transform cameraRigTransform; // Tranform of [Camera Rig]
+        private Transform headTransform; // Player's head transform
+        
+        // Prefab Instances
+        private GameObject laser; // Stores reference to an instance of a laser
+        private GameObject reticle; // Instance of reticle
+
+        private bool shouldTeleport; // True when valid teleport location is found
+        private Vector3 hitPoint; // Hit point of the laser raycast
+        
+        protected override void Awake()
+        {
+            base.Awake();
+            trigger = true;
+            grip = true;
+            appMenu = true;
+            press = true;
+            touch = true;
+
+            SteamVR_ControllerManager controllerManager = FindObjectOfType<SteamVR_ControllerManager>();
+            cameraRigTransform = controllerManager.transform;
+            headTransform = controllerManager.GetComponent<Camera>().transform;
+        }
+
+        private void Start()
+        {
+            laser = Instantiate(laserPrefab);
+            reticle = Instantiate(teleportReticlePrefab);
+        }
+
+        // Trigger
+        protected override void OnTrigger()
+        {
+            // Selection Logic.
+        }
+        
+        
+        // Grip
+        protected override void OnGrip()
+        {
+            // Hide laser and reticle when button is released.
+            laser.SetActive(false);
+            reticle.SetActive(false);
+
+            if (shouldTeleport)
+            {
+                Teleport(hitPoint);
+            }
+        }
+
+        protected override void UpdateGrip()
+        {
+            RaycastHit hit;
+            // Shoot ray from controller, if it hits something store the point where it hit and show laser
+            if (Physics.Raycast(trackedObj.transform.position, transform.forward, out hit, 100))
+            {
+                // Found valid teleport location
+                DrawLaser(hit);
+                hitPoint = hit.point;
+                shouldTeleport = true;
+            }
+            else
+            {
+                // Hide laser and reticle when no valid target is found.
+                laser.SetActive(false);
+                reticle.SetActive(false);
+            }
+        }
+
+        // Draws the laser and reticle.
+        private void DrawLaser(RaycastHit hit)
+        {
+            // Show laser and Reticle
+            laser.SetActive(true);
+            reticle.SetActive(true);
+            
+            // Put laser between controller and where raycast hits
+            laser.transform.position = Vector3.Lerp(trackedObj.transform.position, hit.point, .5f);
+
+            // Point laser at position where raycast hit
+            laser.transform.LookAt(hit.point);
+
+            // Scale laser so it fits between the two positions
+            laser.transform.localScale = new Vector3(laser.transform.localScale.x, laser.transform.localScale.y, hit.distance);
+            
+            // Move the reticle to where the raycast hit, with an offset to avoid z-fighting
+            reticle.transform.position = hit.point + new Vector3(0, 0, RETICLE_OFFSET);
+        }
+        
+        // Teleports the player to the target location.
+        private void Teleport(Vector3 target)
+        {
+            shouldTeleport = false;
+            
+            var difference = cameraRigTransform.position - headTransform.position;
+
+            difference.y = 0;
+
+            cameraRigTransform.position = target + difference;
+        }
+        
+        
+        // Application Menu
+        protected override void OnAppMenu()
+        {
+            // Menu Logic.
+        }
+
+        
+        // Touchpad Press
+        protected override void UpdateTouchpadPress(Vector2 pos)
+        {
+            if (pos.y > DEADZONE_SIZE)
+            {
+                cameraRigTransform.position += Vector3.up * MOVE_OFFSET;
+            }
+            if (pos.y < -DEADZONE_SIZE)
+            {
+                cameraRigTransform.position += Vector3.down * MOVE_OFFSET;
+            }
+        }
+
+        
+        // Touchpad Touch
+        protected override void UpdateTouchpadTouch(Vector2 pos)
+        {
+            cameraRigTransform.position += pos.y * Vector3.forward * MOVE_OFFSET;
+            cameraRigTransform.position += pos.x * Vector3.right * MOVE_OFFSET;
+        }
+    }
+}
