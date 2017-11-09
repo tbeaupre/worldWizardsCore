7e5244dc405f75a534f67ef4ee8134440f3b2540
Tyler Beaupre <tylerbeaupre4@gmail.com>
2017-11-09 12:30:47 -0500

    Debug messages for ControllerListener

--- core/experimental/controllers/ControllerListener.cs
+++ core/experimental/controllers/ControllerListener.cs
@@ -6,6 +6,7 @@ namespace WorldWizards.core.experimental.controllers
     {
         private SteamVR_TrackedController controller;
         protected Tool tool;
+        private bool debug = false;
 
         protected virtual void Awake()
         {
@@ -41,44 +42,62 @@ namespace WorldWizards.core.experimental.controllers
         #region Listener Functions
         private void OnTriggerClick(object sender, ClickedEventArgs e)
         {
+            DebugMessage("OnTriggerClick", sender);
             tool.OnTriggerClick();
         }
         private void OnTriggerUnclick(object sender, ClickedEventArgs e)
         {
+            DebugMessage("OnTriggerUnclick", sender);
             tool.OnTriggerUnclick();
         }
         private void OnGrip(object sender, ClickedEventArgs e)
         {
+            DebugMessage("OnGrip", sender);
             tool.OnGrip();
         }
         private void OnUngrip(object sender, ClickedEventArgs e)
         {
+            DebugMessage("OnUngrip", sender);
             tool.OnUngrip();
         }
         private void OnMenuClick(object sender, ClickedEventArgs e)
         {
+            DebugMessage("OnMenuClick", sender);
             tool.OnMenuClick();
         }
         private void OnMenuUnclick(object sender, ClickedEventArgs e)
         {
+            DebugMessage("OnMenuUnclick", sender);
             tool.OnMenuUnclick();
         }
         private void OnPadClick(object sender, ClickedEventArgs e)
         {
+            DebugMessage("OnPadClick", sender);
             tool.OnPadClick(new Vector2(e.padX, e.padY));
         }
         private void OnPadUnclick(object sender, ClickedEventArgs e)
         {
+            DebugMessage("OnPadUnclick", sender);
             tool.OnPadUnclick();
         }
         private void OnPadTouch(object sender, ClickedEventArgs e)
         {
+            DebugMessage("OnPadTouch", sender);
             tool.OnPadTouch(new Vector2(e.padX, e.padY));
         }
         private void OnPadUntouch(object sender, ClickedEventArgs e)
         {
+            DebugMessage("OnPadUntouch", sender);
             tool.OnPadUntouch();
         }
         #endregion
+
+        private void DebugMessage(string functionName, object sender)
+        {
+            if (debug)
+            {
+                Debug.Log("ControllerListener::" + functionName + ": " + sender.ToString());
+            }
+        }
     }
 }
