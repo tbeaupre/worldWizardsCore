e4eb9c5616ec4e7041c1487d91b808510561bd3e
Tyler Beaupre <tylerbeaupre4@gmail.com>
2017-11-14 19:38:32 -0500

    Input Manager to choose Desktop or VR

---
+++ core/experimental/controllers/InputManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using worldWizards.core.experimental.controllers.Desktop;
+using worldWizards.core.experimental.controllers.VRControls;
+
+namespace WorldWizards.core.experimental.controllers
+{
+    public class InputManager : MonoBehaviour
+    {
+        private void Awake()
+        {
+            SteamVR_ControllerManager controllerManager = FindObjectOfType<SteamVR_ControllerManager>();
+            if (UnityEngine.VR.VRDevice.isPresent)
+            {
+                Debug.Log("InputManager::Awake(): VR Controls Enabled");
+                controllerManager.left.AddComponent<LeftVRController>();
+                controllerManager.right.AddComponent<RightVRController>();
+            }
+            else
+            {
+                Debug.Log("InputManager::Awake(): Desktop Controls Enabled");
+                gameObject.AddComponent<DesktopController>();
+            }
+        }
+    }
+}
