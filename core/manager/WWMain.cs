8cad3da8ba458ab98e820d59b0af93a1f2deb54f
Brian <brianjkd@gmail.com>
2017-11-17 11:48:01 -0500

    Added a main MonoBehavior which should run before everything else in World Wizards.

---
+++ core/manager/WWMain.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace WorldWizards.core.manager
+{
+    /// <summary>
+    /// This MonoBehavior needs to execute before everything else in World Wizards.
+    /// It is responsible for setting up the Manager Registry.
+    /// </summary>
+    public class WWMain : MonoBehaviour
+    {
+        void Awake()
+        {
+            // setup the manager registry
+            var setupManagerRegistry = ManagerRegistry.Instance;
+        }
+    }
+}
