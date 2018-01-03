23e4160382303beb733ee443ec4fb8f3aca6a6b8
jeffpk@worldwizards.net <>
2018-01-03 14:00:12 -0500

    checking in third party code

---
+++ ThirdParty/WWUtils/Unity/SerializationProxies/Wrappers/SPWType.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+ 
+/// <summary>
+/// A Serialization wrapper around a Type variable
+/// </summary>
+/// 
+
+[System.Serializable]
+public class SPWType : ISerializationCallbackReceiver {
+
+    public SPWType(Type t)
+    {
+        type = t;
+    }
+
+	public Type type
+    {
+        get;
+        private set;
+    }
+
+    [SerializeField]
+    private string fqtn;
+    [SerializeField]
+    private string assemblyName;
+
+
+    public void OnBeforeSerialize()
+    {
+        fqtn = type.FullName;
+        assemblyName = type.Assembly.FullName;
+    }
+
+    public void OnAfterDeserialize()
+    {
+        Assembly assembly = Assembly.Load(assemblyName);
+        type = assembly.GetType(fqtn);
+    }
+}
