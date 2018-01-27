f93ec42cd5848a235a846f6260e3a52ba2222301
Brian <brianjkd@gmail.com>
2018-01-27 12:54:19 -0500

    fixed issue with SaveLoadSceneGraph

--- core/experimental/SaveLoadSceneGraph.cs
+++ core/experimental/SaveLoadSceneGraph.cs
@@ -9,12 +9,6 @@ namespace WorldWizards.core.experimental
 {
     public class SaveLoadSceneGraph : MonoBehaviour
     {
-        private void Start()
-        {
-            WWResourceController.LoadResource("white", null, "whiteCube");
-            WWResourceController.LoadResource("tree", null, "treeProp");
-        }
-
         public void Save()
         {
             ManagerRegistry.Instance.GetAnInstance<SceneGraphManager>().Save(FileIO.testPath);
@@ -23,21 +17,6 @@ namespace WorldWizards.core.experimental
         public void Load()
         {
             ManagerRegistry.Instance.GetAnInstance<SceneGraphManager>().Load(FileIO.testPath);
-        }
-
-        public void CreateMaze()
-        {
-            var imagePath = "Heightmaps/MazeHeightmap";
-            var heightmap = Resources.Load<Texture2D>(imagePath);
-
-            List<Coordinate> terrainCoordinates = TerrainGenerator.CreateTerrainFromImage(heightmap);
-        }
-
-        public void CreateTerrain()
-        {
-            var imagePath = "Heightmaps/TerrainHeightmap";
-            var heightmap = Resources.Load<Texture2D>(imagePath);
-            List<Coordinate> terrainCoordinates = TerrainGenerator.CreateTerrainFromImage(heightmap);
         }
 
         public void DeleteObjects()
